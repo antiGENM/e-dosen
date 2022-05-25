4c006fd3331de76149e53ee9be473493934cb59a
antiGENM <lukasbelipati@students.unnes.ac.id>
2022-05-26 01:05:02 +0700

    update halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,33 @@
         <link rel="stylesheet" href="css/mdb.min.css" />
     </head>
     <body>
+        <!--Main Navigation-->
+        <style>
+        #intro {
+            background-image: url(https://p4.wallpaperbetter.com/wallpaper/590/223/706/design-flower-line-wallpaper-preview.jpg);
+            height: 100vh;
+        }
+        </style>
 
+        <!-- Background image -->
+        <div id="intro" class="bg-image">
+            <div class="mask d-flex align-items-center h-100" style="background-color: rgba(0, 0, 0, 0.8)">
+                <div class="container">
+                    <div class="row justify-content-center row-xl-9">
+                        <div class="col-xl-9 col-md-8">
+                        
+                        <form class="bg-white rounded shadow-5-strong p-5"> 
+                        <h3 class="text-center font-weight-light">E-Dosen</h3>
+                            
+
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- MDB -->
+        <script type="text/javascript" src="js/mdb.min.js"></script>
     </body>
+</html>    
