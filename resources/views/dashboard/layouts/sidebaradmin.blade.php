f5c77b1868ca83cbce1deacea56727b72bef2afc
antiGENM <lukasbelipati@students.unnes.ac.id>
2022-05-25 20:49:27 +0700

    first

---
+++ resources/views/dashboard/layouts/sidebaradmin.blade.php
@@ -0,0 +1,24 @@
+      <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+          <div class="sb-sidenav-menu">
+            <div class="nav ">
+              <a class="nav-link " href="/dashboard admin">
+                <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
+                Dashboard
+              </a>
+              <a class="nav-link" href="/listdosen">
+                <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                Daftar Dosen
+              </a>
+              <a class="nav-link" href="/listposts">
+                <div class="sb-nav-link-icon">&nbsp;<i class="fas fa-file-lines"></i></div>
+                &nbsp;Post Blog
+              </a>
+            </div>
+          </div>
+          <div class="sb-sidenav-footer ">
+            <div class="small">E-Dosen</div>
+            RPL
+          </div>
+        </nav>
+      </div>
---
+++ resources/views/dashboard/layouts/sidebardosen.blade.php
@@ -0,0 +1,24 @@
+<div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+          <div class="sb-sidenav-menu">
+            <div class="nav">
+              <a class="nav-link" href="/dashboard dosen">
+                <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
+                Dashboard
+              </a>
+              <a class="nav-link" href="/mydata">
+                <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
+                My Data
+              </a>
+              <a class="nav-link" href="/membership">
+                <div class="sb-nav-link-icon"><i class="fas fa-address-card"></i></div>
+                Membership
+              </a>
+            </div>
+          </div>
+          <div class="sb-sidenav-footer">
+            <div class="small">E-Dosen</div>
+            RPL
+          </div>
+        </nav>
+      </div>
