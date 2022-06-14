effbaf0a737facb78f4cbfdfa3093a24298e8ea1
antiGENM <lukasbelipati@students.unnes.ac.id>
2022-06-14 16:50:49 +0700

    membetulkan + merapikan kodingan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,23 +26,22 @@
       <div class="mask d-flex align-items-center h-100" style="background-color: rgba(84, 88, 94, 0.8)">
         <div class="container">
           <div class="row justify-content-center">
-            <div class="col-xl-5 col-md-8">
-             
-            <form class="bg-white rounded shadow-5-strong p-5"> 
-            <h3 class="text-center font-weight-light">Logo</h3>
+            <div class="col-xl-5 col-md-8">             
+              <form class="bg-white rounded shadow-5-strong p-5"> 
+                <h3 class="text-center font-weight-light">Logo</h3>
                 <h3 class="text-center font-weight-light my-4">Login</h3> 
                 <div class="form-floating mb-3">
-                        <input class="form-control"  id="Name" type="text" placeholder=""/>
-                        <label for="inputName">Username</label>
-                    </div>               
-                    <div class="form-floating mb-3">
-                        <input class="form-control"  id="Name" type="password" placeholder="" />
-                        <label for="inputName">Password</label>
-                    </div> 
+                  <input name="name" class="form-control"  id="name" type="text" placeholder="Name"/>
+                  <label for="name">Username</label>
+                </div>               
+                <div class="form-floating mb-3">
+                  <input name="password" class="form-control"  id="password" type="password" placeholder="Password" />
+                  <label for="password">Password</label>
+                </div> 
 
                 <!-- Submit button -->
                 <div class="d-grid">
-                    <a class="btn btn-primary btn-lg mb-3" href="/dashboard dosen">Login</a>
+                  <a class="btn btn-primary btn-lg mb-3" type="submit">Login</a>
                 </div>
                 <div class="text-center">
                   <p>Belum memiliki akun? <a href="/register">Sign Up</a></p>
@@ -51,25 +50,24 @@
                       <hr>
                     </div>
                     <div class="col-2">
-                    <p>atau</p>
+                      <p>atau</p>
                     </div>
                     <div class="col">
                       <hr>
                     </div>               
-                  </div>
-                
+                  </div>                  
                 </div>
                 <div class="d-grid">
-                    <a class="btn btn-lg" href="/dashboard" >
-                      <div class="row row-cols-auto justify-content-center" >
-                        <div class="col-3">
+                  <a class="btn btn-lg" href="/dashboard" >
+                    <div class="row row-cols-auto justify-content-center" >
+                      <div class="col-3">
                         <img src="img/google.png" alt="" style="width: 20px; height: 20px;">
-                        </div>
-                        <div class="col">
-                          Login dengan Google
-                        </div>
                       </div>
-                    </a>
+                      <div class="col">
+                        Login dengan Google
+                      </div>
+                    </div>
+                  </a>
                 </div>
               </form>
             </div>
